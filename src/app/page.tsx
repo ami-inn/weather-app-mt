f4a13a3b9e8cda970120e77768f98d9bed95c1ee
ami-inn <amipk2001@gmail.com>
2025-07-23 17:10:52 +0530

    navbar component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,36 +9,12 @@ import {
 } from "lucide-react";
 import WeatherImg from "../assets/weather.png";
 import Image from "next/image";
+import Navbar from "@/shared/components/Navbar";
 
 const page = () => {
   return (
     <main className="flex flex-col min-h-screen px-32 py-8  items-center">
-      <nav className="flex items-center w-full justify-between px-4 py-2 bg-gray-100">
-        {/* Left side: Bold text */}
-        <h2 className="text-2xl font-bold text-gray-800">Amin</h2>
-        {/* Right side: Toggle, Search bar */}
-        <div className="flex items-center gap-4">
-          {/* Search Bar */}
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search"
-              className="pl-3 pr-10 py-1 rounded border border-gray-300 focus:outline-none"
-            />
-            <span className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400">
-              {/* Lucide: Search icon */}
-              <LucideSearch className="w-5 h-5" />
-            </span>
-          </div>
-          {/* Toggle Icon */}
-          <button className="p-2 rounded hover:bg-gray-200">
-            <svg className="w-6 h-6" aria-hidden="true">
-              {/* Example toggle icon (Lucide: Menu) */}
-              <LucideMenu />
-            </svg>
-          </button>
-        </div>
-      </nav>
+     <Navbar/>
       {/* weathre info section */}
       <section className="w-full flex justify-center mt-2">
         <div className="relative w-full ">
---
+++ src/shared/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { LucideMenu, LucideSearch } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center w-full justify-between py-2 ">
+      {/* Left side: Bold text */}
+      <h2 className="text-3xl font-bold text-gray-800">Amin</h2>
+      {/* Right side: Toggle, Search bar */}
+      <div className="flex items-center gap-4">
+        {/* Search Bar */}
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search"
+            className="pl-3 px-3 placeholder:text-[#A8ABC4] text-[#A8ABC4]  pr-10 py-2 rounded border border-secondary focus:outline-none"
+          />
+          <span className="absolute right-3 top-1/2 -translate-y-1/2 text-secondary">
+            <LucideSearch className="w-5 h-5" />
+          </span>
+        </div>
+        {/* Toggle Icon */}
+        {/* <button className="p-2 rounded hover:bg-gray-200">
+          <svg className="w-6 h-6" aria-hidden="true">
+            <LucideMenu />
+          </svg>
+        </button> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
